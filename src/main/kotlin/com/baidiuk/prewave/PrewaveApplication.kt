0cfb660f338314946a78c648b64a1145722ffc17
Yevhen Baidiuk <y.baidiuk@gmail.com>
2025-04-23 18:36:36 +0200

    initial commit

---
+++ src/main/kotlin/com/baidiuk/prewave/PrewaveApplication.kt
@@ -0,0 +1,11 @@
+package com.baidiuk.prewave
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrewaveApplication
+
+fun main(args: Array<String>) {
+    runApplication<PrewaveApplication>(*args)
+}
---
+++ src/test/kotlin/com/baidiuk/prewave/PrewaveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.baidiuk.prewave
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrewaveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
